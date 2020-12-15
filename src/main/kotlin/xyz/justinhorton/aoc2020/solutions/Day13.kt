4b1918b6c5e300c610caf2e5d7186d6bc7be4e27
Justin Horton <18197817+justinhorton@users.noreply.github.com>
2020-12-15 12:08:47 -0800

    Day 13, part 2

--- src/main/kotlin/xyz/justinhorton/aoc2020/solutions/Day13.kt
+++ src/main/kotlin/xyz/justinhorton/aoc2020/solutions/Day13.kt
@@ -2,9 +2,7 @@ package xyz.justinhorton.aoc2020.solutions
 
 import xyz.justinhorton.aoc2020.template.Day
 import xyz.justinhorton.aoc2020.template.Solution
-import kotlin.math.abs
 import kotlin.math.ceil
-import kotlin.math.roundToInt
 
 private typealias BusId = Int
 private typealias Timestamp = Int
@@ -12,35 +10,64 @@ private typealias Timestamp = Int
 /**
  * [Advent of Code - Day 13](https://adventofcode.com/2020/day/13)
  */
-class Day13(input: String) : Day<Pair<Timestamp, List<BusId>>, Int, Int> {
-    override val parsedInput: Pair<Int, List<Int>> by lazy {
+class Day13(input: String, val minT: Long = 0L) : Day<Pair<Timestamp, List<Day13.BusConstraint>>, Int, Long> {
+    override val parsedInput: Pair<Int, List<BusConstraint>> by lazy {
         val lines = input.trim().lines()
         val earliestTimestamp = lines[0].toInt()
 
-        val busIds = lines[1].split(",").mapNotNull {
-            it.toIntOrNull()
+        val busConstraints: List<BusConstraint> = lines[1].split(",").map {
+            val busId: BusId? = it.toIntOrNull()
+            busId?.let { parsedId -> BusIdConstraint(parsedId) } ?: AnyBusConstraint
         }
-        Pair(earliestTimestamp, busIds)
+        Pair(earliestTimestamp, busConstraints)
     }
 
     override val part1Solution: Solution<Int> = Solution {
         val (earliestTimestamp, busIds) = parsedInput
 
-        val result: List<Pair<Int, BusId>> = busIds.asSequence()
+        val earliestDepartureAndBusId: Pair<Int, BusId> = busIds.asSequence()
+            .filterIsInstance<BusIdConstraint>()
+            .map { it.busId }
             .map { id ->
                 val earliestDeparture = ceil(earliestTimestamp / id.toDouble()).toInt() * id
                 (earliestDeparture - earliestTimestamp) to id
             }
             .sortedBy { it.first }
-            .toList()
+            .first()
 
-        val r = result.first()
-        r.second * r.first
+        val (timestamp, busId) = earliestDepartureAndBusId
+        timestamp * busId
     }
 
-    override val part2Solution: Solution<Int> = Solution {
-        TODO()
+    /**
+     * [https://en.wikipedia.org/wiki/Chinese_remainder_theorem](Chinese Remainder Theorem)
+     */
+    override val part2Solution: Solution<Long> = Solution {
+        val withOffsets: Array<BusInputOffsetConstraint> = parsedInput.second.mapIndexedNotNull { offset, busConstraint ->
+            (busConstraint as? BusIdConstraint)?.let { BusInputOffsetConstraint(it.busId, offset) }
+        }.toTypedArray()
+
+        var solution = 0L
+        var lcd = 1L
+        withOffsets.forEach { bc ->
+            while ((solution + bc.offset) % bc.busId != 0L) {
+                solution += lcd
+            }
+
+            lcd *= bc.busId
+        }
+        solution
     }
+
+    interface BusConstraint
+
+    data class BusIdConstraint(val busId: BusId) : BusConstraint
+
+    data class BusInputOffsetConstraint(val busId: BusId, val offset: Int) : BusConstraint {
+        fun departsAt(t: Long): Boolean = t % busId == 0L
+
+        fun nextDepartureAfter(t: Long): Long = ceil(t / busId.toDouble()).toLong() * busId
+    }
+
+    object AnyBusConstraint : BusConstraint
 }
-
-
--- src/test/kotlin/xyz/justinhorton/aoc2020/solutions/Day13Test.kt
+++ src/test/kotlin/xyz/justinhorton/aoc2020/solutions/Day13Test.kt
@@ -3,6 +3,7 @@ package xyz.justinhorton.aoc2020.solutions
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertNotEquals
 
 class Day13Test {
     private lateinit var day: Day13
@@ -10,7 +11,7 @@ class Day13Test {
     @BeforeEach
     fun beforeEach() {
         val inFile = Day13Test::class.java.getResource("input-day13.txt")
-        day = Day13(inFile.readText())
+        day = Day13(inFile.readText(), 100000000000000L)
     }
 
     @Test
@@ -20,18 +21,19 @@ class Day13Test {
 
     @Test
     fun `test part 1`() {
+        assertNotEquals(23767383291910, day.part1Solution.computeAnswer())
         assertEquals(171, day.part1Solution.computeAnswer())
     }
 
-//    @Test
-//    fun `test part 2 sample`() {
-//        assertEquals(286, Day13(SAMPLE).part2Solution.computeAnswer())
-//    }
+    @Test
+    fun `test part 2 sample`() {
+        assertEquals(1068781, Day13(SAMPLE).part2Solution.computeAnswer())
+    }
 
-//    @Test
-//    fun `test part 2`() {
-//        assertEquals(42908, day.part2Solution.computeAnswer())
-//    }
+    @Test
+    fun `test part 2`() {
+        assertEquals(539746751134958, day.part2Solution.computeAnswer())
+    }
 }
 
 private val SAMPLE =
